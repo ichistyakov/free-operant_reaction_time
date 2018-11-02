d13cf59d2f199a7ee4e70355e85539cf36dd23df
NSAkela <ichistyakov@hse.ru>
2018-11-03 01:27:10 +0300

    Removed variable KITTY Updated comments Button and schedule of reinforcement was added

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ COLORDICT = {
 
 # Controls general flow of the program
 def main():
-    global FPSCLOCK, DISPLAYSURF, BASICFONT, KITTY
+    global FPSCLOCK, DISPLAYSURF, BASICFONT
 
     pygame.init()
     FPSCLOCK = pygame.time.Clock()
@@ -36,9 +36,14 @@ def main():
 
 # Controls flow of the experiment
 def experiment():
-    global CHANGE_STIMULI
+    global CHANGE_STIMULI, DATA
 
-    phase = next(PHASES)
+    CHANGE_STIMULI = pygame.USEREVENT + 1
+    DATA = ['time', 'responses', 'phase_name', 'phase_id']
+    phase = {
+        'name': next(PHASES),
+        'id': 1,
+    }
     stimulus = {
         'text': 'white',
         'color': 'white',
@@ -46,60 +51,79 @@ def experiment():
     }
     button = {
         'bounds': pygame.Rect(350, 450, 100, 100),
-        'color': BUTTON_NORMAL_BACK_COLOR,
-        'on_click': None
+        'state': 'normal'
     }
-    CHANGE_STIMULI = pygame.USEREVENT + 1
-    pygame.time.set_timer(CHANGE_STIMULI, 1000)
+    reinforcement = {
+        'count': 0,
+        'available': True
+    }
 
+    pygame.time.set_timer(CHANGE_STIMULI, 1000)
     while True:
         DISPLAYSURF.fill(BACKGROUND)
         # Updates states
         for event in pygame.event.get():
             if event.type == QUIT:
                 terminate()
-            stimulus = stimuli_handler(event, phase, **stimulus)
-            phase = phase_handler(phase, event)
-            button = button_handler(event, **button)
+            phase = phase_handler(event, **phase)
+            stimulus = stimuli_handler(event, phase['name'], **stimulus)
+            button = button_state_handler(event, **button)
+            reinforcement = reinforcement_handler(button['state'], event, **reinforcement)
         # Draws states
-        text_stimuli(**stimulus)
+        text_stimulus(**stimulus)
         button_object(**button)
         # Updates screen
         pygame.display.update()
         FPSCLOCK.tick(FPS)
 
 
-# Check events and updates state of the button
-def button_handler(ivent, **kwargs):
-    bounds = kwargs['bounds']
-    color = kwargs['color']
-    on_click = kwargs['on_click']
-    if ivent.type == pygame.MOUSEMOTION:
-        if bounds.collidepoint(ivent.pos):
-            color = BUTTON_HOVER_BACK_COLOR
-        else:
-            color = BUTTON_NORMAL_BACK_COLOR
-    elif ivent.type == pygame.MOUSEBUTTONDOWN:
-        if ivent.button == 1:
-            if bounds.collidepoint(ivent.pos):
-                color = BUTTON_PRESSED_BACK_COLOR
-    elif ivent.type == pygame.MOUSEBUTTONUP:
-        if ivent.button == 1:
-            if bounds.collidepoint(ivent.pos):
-                color = BUTTON_HOVER_BACK_COLOR
+# Checks button state and updates score. Only one correct response per stimuli is reinforced.
+def reinforcement_handler(button_state, ivent, **current_state):
+    count = current_state['count']
+    available = current_state['available']
+    if available:
+        if button_state == 'pressed':
+            count += 1
+            available = False  # Reinforcement becomes unavailable until nearest stimuli change
+    if ivent.type == CHANGE_STIMULI:
+        available = True
     new_state = {
-        'bounds': bounds,
-        'color': color,
-        'on_click': on_click
+        'count': count,
+        'available': available
     }
     return new_state
 
 
-# Generates new stimuli configuration every CHANGE_STIMULI based on current phase or does nothing
-def stimuli_handler(ivent, phase, **kwargs):
-    text = kwargs['text']
-    color = kwargs['color']
-    pos = kwargs['pos']
+# Checks events and updates the state of the button
+def button_state_handler(ivent, **current_state):
+    bounds = current_state['bounds']
+    state = current_state['state']
+    if ivent.type == pygame.MOUSEMOTION:
+        if bounds.collidepoint(ivent.pos):
+            state = 'hover'
+        else:
+            state = 'normal'
+    elif ivent.type == pygame.MOUSEBUTTONDOWN:
+        if ivent.button == 1:
+            if bounds.collidepoint(ivent.pos):
+                state = 'pressed'
+    elif ivent.type == pygame.MOUSEBUTTONUP:
+        if ivent.button == 1:
+            if bounds.collidepoint(ivent.pos):
+                state = 'hover'
+    new_state = {
+        'bounds': bounds,
+        'state': state
+    }
+    return new_state
+
+
+# Generates new stimuli configuration every CHANGE_STIMULI
+# Produced stimuli depend on the current phase
+def stimuli_handler(ivent, phase, **stim_parameters):
+    text = stim_parameters['text']
+    color = stim_parameters['color']
+    pos = stim_parameters['pos']
     if ivent.type == CHANGE_STIMULI:
         if phase == 'A':
             text = random.choice(WORDS)
@@ -118,29 +142,46 @@ def stimuli_handler(ivent, phase, **kwargs):
     return new_state
 
 
-# Alternates between values that coded in 'phases' or does nothing. Feel free to add rules here.
-def phase_handler(current_state, ivent):
+# Receives current phase name and id
+# If Q is pressed - returns next name and id from infinite iterator PHASES (defined at the beginning)
+def phase_handler(ivent, **current_state):
+    name = current_state['name']
+    id = current_state['id']
     if ivent.type == KEYDOWN and ivent.key == K_q:
-        return next(PHASES)
-    else:
-        return current_state
+        name = next(PHASES)
+        id += 1
+    new_state = {
+        'name': name,
+        'id': id
+    }
+    return new_state
 
 
-# Accepts a dict of parameters and modifies the global surface. This side-effect is intended.
+# Receives a dict of button parameters and modifies the global surface
+# This side effect is intended
 def button_object(**kwargs):
     bounds = kwargs['bounds']
-    color = kwargs['color']
+    state = kwargs['state']
+    color_dict = {
+        'normal': BUTTON_NORMAL_BACK_COLOR,
+        'hover': BUTTON_HOVER_BACK_COLOR,
+        'pressed': BUTTON_PRESSED_BACK_COLOR,
+        'relaxed': BUTTON_HOVER_BACK_COLOR
+    }
+    color = color_dict.get(state)
     pygame.draw.rect(DISPLAYSURF, color, bounds)
 
 
-# Modifies the global surface, but this side effect is intended
-def text_stimuli(**kwargs):
+# Receives a dict of stimulus paramaters and modifies the global surface
+# This side effect is intended
+def text_stimulus(align = True, **kwargs):
     pos = kwargs['pos']
     text = kwargs['text']
     color = kwargs['color']
     font = BASICFONT
     text_surface, bounds = get_surface(font, text, COLORDICT.get(color))
-    pos = centralize(pos, bounds.width)
+    if align:
+        pos = centralize(pos, bounds.width)
     DISPLAYSURF.blit(text_surface, pos)
 
 
@@ -155,10 +196,11 @@ def get_surface(font, text, color):
     return text_surface, text_surface.get_rect()
 
 
+# Finishes program
 def terminate():
     pygame.quit()
     sys.exit()
 
-
+# Starts program
 if __name__ == '__main__':
     main()
